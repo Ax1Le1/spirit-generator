40621e6cdb75eb7de688f5d8e58cea3eb64c1491
yaojc <1186948824@qq.com>
2024-03-13 09:14:25 +0800

    程序构建jar包

--- spirit-generator-maker/src/main/java/com/spirit/maker/generator/MainGenerator.java
+++ spirit-generator-maker/src/main/java/com/spirit/maker/generator/MainGenerator.java
@@ -82,6 +82,12 @@ public class MainGenerator {
         outputFilePath = outputBaseJavaPackagePath + "/generator/FileGenerator.java";
         DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
 
+
+        // generator.MainGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/pom.xml.ftl";
+        outputFilePath = outputPath  + File.separator + "pom.xml";
+        DynamicFileGenerator.doGenerate(inputFilePath, outputFilePath, meta);
+
         //构建jar包
         JarGenerator.doGenerate(outputPath);
     }
